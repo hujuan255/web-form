163c854c7b0b0e80abac5d1440b0bb9f9f3622c8
用户名 <hujuan@kaishengit.com>
2022-04-24 10:57:40 +0800

    更新表单提交

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,5 +1,6 @@
 <template>
-  <form>
+  <!-- @submit.prevent 提交事件不再重载页面 -->
+  <form @submit.prevent="handleSubmit">
     <label>Email</label>
     <!-- 类型为email，required 必填项 -->
     <!-- v-model 实现数据，用户表达填充双向绑定 -->
@@ -7,6 +8,7 @@
     <label>Password</label>
     <!-- 类型为email，required 必填项 -->
     <input type="password" required v-model="password" />
+    <div class="error" v-if="passwordError">{{ passwordError }}</div>
     <label>roel:</label>
     <!-- v-model绑定下拉列表框 -->
     <select v-model="roel">
@@ -18,9 +20,12 @@
       <label> 我同意 </label>
     </div>
     <label>Skill:</label>
-    <input type="text" v-model="temSkill" @keyup.enter="getSkills" />
+    <input type="text" v-model="temSkill" @keyup="addSkill" />
     <div class="skills" v-for="(skill, index) in skills" :key.alt="index">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+    <div class="submitForm">
+      <button>提交</button>
     </div>
   </form>
   <p>email:{{ email }}</p>
@@ -40,21 +45,42 @@ export default {
       agree: false,
       skills: [],
       temSkill: "",
+      passwordError: "",
     };
   },
   methods: {
     // 获取输入框的值,按下“，”添加到数组中
-    getSkills(e) {
+    addSkill(e) {
       // 判断是否按下“，”并且输入框中有临时值
-      if (this.temSkill) {
+      if (e.key === "," && this.temSkill) {
         //判断数组中是否已经包含该值，不能重复输入。includes():判断数组中是否包含某个值。如果包含返回true，否则返回false
         if (!this.skills.includes(this.temSkill)) {
           // 如果比包含该值，则用push():向数组中添加一个或多个元素，并返回新的长度。
-          this.skills.push(this.temSkill);
+          this.skills.push(this.temSkill.slice(0, -1));
         }
         // 清空输入框的临时值
         this.temSkill = "";
       }
+    },
+    //删除技能
+    deleteSkill(skill) {
+      // 删除数组中的某个值，filter():返回一个新数组，新数组中的元素是通过测试函数返回true，保留该元素，false则不保留。
+      this.skills = this.skills.filter((item) => {
+        return item != skill;
+      });
+    },
+    // 提交表单
+    handleSubmit() {
+      //判断密码的长度是否大于6位
+      this.passwordError =
+        this.password.length < 6 ? "密码长度不能小于6位" : "";
+      console.log(
+        this.email,
+        this.password,
+        this.roel,
+        this.agree,
+        this.skills
+      );
     },
   },
 };
@@ -88,8 +114,8 @@ select {
     注意: 内边距、边框和外边距都在这个盒子的外部*/
   box-sizing: border-box;
   border: none;
-  border-bottom: 1px solid #ccc;
-  color: #555;
+  border-bottom: 1px solid #ddd;
+  color: #888;
 }
 input[type="checkbox"] {
   width: 30px;
@@ -107,7 +133,23 @@ input[type="checkbox"] {
   font-size: 12px;
   font-weight: 900;
   border-radius: 20px;
-  /* 鼠标悬停呈现手的样式 */
+  /* 鼠标悬停呈现手的样式  */
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  color: #fff;
+  margin-top: 20px;
+  border-radius: 20px;
+}
+.submitForm {
+  text-align: center;
+}
+.error {
+  color: red;
+  margin-top: 10px;
+  font-size: 0.8em;
 }
 </style>
